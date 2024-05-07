f929b7c3e547c1b85eb18e7c56b4b5c89dadb977
hthuz <hanggang.20@intl.zju.edu.cn>
2024-05-07 16:27:39 +0800

    add: item device clicker

--- app/src/main/java/com/hthuz/lockcompanion/MyDeviceAdapter.java
+++ app/src/main/java/com/hthuz/lockcompanion/MyDeviceAdapter.java
@@ -1,19 +1,24 @@
 package com.hthuz.lockcompanion;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.hthuz.lockcompanion.databinding.ItemDeviceBinding;
+import com.hthuz.lockcompanion.ui.home.HomeFragment;
 
 import java.util.List;
 
 public class MyDeviceAdapter extends RecyclerView.Adapter<MyDeviceAdapter.ViewHolder> {
 
     private final List<MyDevice> lists;
+    private final static String TAG = "DEBUG";
 
     public MyDeviceAdapter(List<MyDevice> lists) {
         this.lists = lists;
@@ -48,14 +53,21 @@ public class MyDeviceAdapter extends RecyclerView.Adapter<MyDeviceAdapter.ViewHo
         public ViewHolder(@NonNull ItemDeviceBinding itemTextDataRvBinding) {
             super(itemTextDataRvBinding.getRoot());
             binding = itemTextDataRvBinding;
-            binding.tvName.setOnClickListener(v -> {
-                binding.tvName.append(" Clicked!");
+            binding.deviceItem.setOnClickListener(v -> {
+                MyDevice device = binding.getDevice();
+                String name = device.getDevice().getName() != null ? device.getDevice().getName().toString() : "Unknown";
+                String address = device.getDevice().getAddress().toString();
+                if (!name.contains("ESP32")) {
+                    showMsg("Only door lock can be connected");
+                }
+                Log.i(TAG, name);
+                Log.i(TAG, device.getDevice().getAddress());
             });
         }
 
-        private void showMsg(CharSequence msg) {
-//            Toast.makeText(, msg,Toast.LENGTH_SHORT).show();
-//            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        public void showMsg(CharSequence msg) {
+            Toast.makeText(this.itemView.getContext(), msg, Toast.LENGTH_SHORT).show();
         }
+
     }
 }
