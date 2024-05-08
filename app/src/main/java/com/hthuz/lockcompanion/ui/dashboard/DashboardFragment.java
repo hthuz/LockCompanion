16d410b626eb57c24ef664134fc18e6bee4f5f13
hthuz <hanggang.20@intl.zju.edu.cn>
2024-05-08 20:38:09 +0800

    refactor: move service connect and readRssiThread into ViewModel

--- app/src/main/java/com/hthuz/lockcompanion/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/hthuz/lockcompanion/ui/dashboard/DashboardFragment.java
@@ -77,23 +77,69 @@ public class DashboardFragment extends Fragment {
 
         requestBluetoothConnect = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
             Intent gattServiceIntent = new Intent(getActivity(), BluetoothLeService.class);
-            getActivity().bindService(gattServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+            getActivity().bindService(gattServiceIntent, getViewModel().getServiceConnection(), Context.BIND_AUTO_CREATE);
         });
 
         getActivity().registerReceiver(gattUpdateReceiver,makeGattUpdateIntentFilter());
 
-        readRssiThread.start();
+
+        if (getViewModel().getReadRssiThread().getState() == Thread.State.NEW) {
+            getViewModel().getReadRssiThread().start();
+        }
+
+        getViewModel().getReadRssiThread().startThread();
         // bluetooth related
+        Log.i(TAG, "connected is " + getViewModel().isConnected());
 
         initView();
         return root;
     }
 
+    private final BroadcastReceiver gattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+//            Log.e(TAG, "Recevie broadcasted action: " + action);
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                getViewModel().setConnected(true);
+                // show connected message
+                getViewModel().setState("CONNECTED");
+                binding.connectState.setText(getViewModel().getState());
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                getViewModel().setConnected(false);
+                // show disconnected message
+                getViewModel().setState("DISCONNECTED");
+                binding.connectState.setText(getViewModel().getState());
+
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    Log.e(TAG, "Error");
+                }
+                final boolean result = getViewModel().getBluetoothService().connect(Values.macAddress);
+                Log.d(TAG, "Connect request result=" + result);
+            } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
+                // display supported services
+                getViewModel().displayGattServices(getViewModel().getBluetoothService().getSupportedGattServices());
+            } else if (BluetoothLeService.ACTION_CHARACTERISTIC_READ.equals(action)
+                    || BluetoothLeService.ACTION_CHARACTERISTIC_CHANGED.equals(action)) {
+                String readValue = intent.getStringExtra("readValue");
+                binding.readConsole.setText("Read Data:" + readValue);
+                Log.e(TAG, "fragment read: " + readValue);
+            } else if (BluetoothLeService.ACTION_READ_REMOTE_RSSI.equals(action)) {
+                int rssi = intent.getIntExtra("rssi", 0);
+                binding.rssiValue.setText("rssi: " + rssi);
+                if (getViewModel().isConnected() && rssi < 0 && rssi > -45) {
+                    writeESP32("5");
+                }
+            }
+        }
+    };
     @Override
     public void onDestroyView() {
         Log.i(TAG, "Dashboard fragment onDestroyView");
         super.onDestroyView();
-        readRssiThread.stopThread();
+        getViewModel().getReadRssiThread().stopThread();
         binding = null;
     }
     @Override
@@ -112,100 +158,20 @@ public class DashboardFragment extends Fragment {
     //        public String testMAC = "14:94:6C:C0:53:83"; // My iP
 //
 //    public String testMac = Values.macAddress;
-    public UUID ESP32_USER_SERVICE_UUID = UUID.fromString("12a59900-17cc-11ec-9621-0242ac130002");
-    public UUID ESP32_RX_CHARACTERISTIC_UUID = UUID.fromString("12a59e0a-17cc-11ec-9621-0242ac130002");
-    public UUID ESP32_TX_CHARACTERISTIC_UUID = UUID.fromString("12a5a148-17cc-11ec-9621-0242ac130002");
+    public static UUID ESP32_USER_SERVICE_UUID = UUID.fromString("12a59900-17cc-11ec-9621-0242ac130002");
+    public static UUID ESP32_RX_CHARACTERISTIC_UUID = UUID.fromString("12a59e0a-17cc-11ec-9621-0242ac130002");
+    public static UUID ESP32_TX_CHARACTERISTIC_UUID = UUID.fromString("12a5a148-17cc-11ec-9621-0242ac130002");
 
-    private BluetoothLeService bluetoothService;
-    private boolean connected;
-    private Thread readRssiThread = new Thread() {
-        @Override
-        public void run() {
-            while(true) {
-                if (connected) {
-                    try {
-                        sleep(500);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    Log.i(TAG, "LOOP");
-                    bluetoothService.readRemoteRssi();
-                }
-            }
-        }
-    };
 
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            Log.i(TAG, "onServiceConnected");
-            bluetoothService = ((BluetoothLeService.LocalBinder) iBinder).getService();
-            if ( bluetoothService != null) {
-                if (!bluetoothService.initialize()) {
-                    Log.e(TAG, "Unable to initialize Bluetooth");
-                    getActivity().finish();
-                }
-                // perform connection
-                final boolean result = bluetoothService.connect(Values.macAddress);
-                Log.d(TAG, "Connect request result=" + result);
-            }
-
-        }
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            bluetoothService = null;
-        }
-    };
-    private final BroadcastReceiver gattUpdateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-//            Log.e(TAG, "Recevie broadcasted action: " + action);
-            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
-                connected = true;
-                // show connected message
-                getViewModel().setState("CONNECTED");
-                binding.connectState.setText(getViewModel().getState());
-            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
-                connected = false;
-                // show disconnected message
-                getViewModel().setState("DISCONNECTED");
-                binding.connectState.setText(getViewModel().getState());
-
-                try {
-                    Thread.sleep(2000);
-                } catch (Exception e) {
-                    Log.e(TAG, "Error");
-                }
-                final boolean result = bluetoothService.connect(Values.macAddress);
-                Log.d(TAG, "Connect request result=" + result);
-            } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
-                // display supported services
-                displayGattServices(bluetoothService.getSupportedGattServices());
-            } else if (BluetoothLeService.ACTION_CHARACTERISTIC_READ.equals(action)
-                    || BluetoothLeService.ACTION_CHARACTERISTIC_CHANGED.equals(action)) {
-                String readValue = intent.getStringExtra("readValue");
-                binding.readConsole.setText("Read Data:" + readValue);
-                Log.e(TAG, "fragment read: " + readValue);
-            } else if (BluetoothLeService.ACTION_READ_REMOTE_RSSI.equals(action)) {
-                int rssi = intent.getIntExtra("rssi", 0);
-                binding.rssiValue.setText("rssi: " + rssi);
-                if (connected && rssi < 0 && rssi > -45) {
-                    writeESP32("5");
-                }
-            }
-        }
-    };
 
     public void startConnect() {
         requestBluetoothConnect.launch(android.Manifest.permission.BLUETOOTH_CONNECT);
     }
 
     private void initView() {
-
         binding.connectState.setText(getViewModel().getState());
         binding.btnConnect.setOnClickListener(v -> {
-            if (connected) {
+            if (getViewModel().isConnected()) {
                 showMsg("DoorLock is Already Connected");
                 return;
             }
@@ -218,28 +184,28 @@ public class DashboardFragment extends Fragment {
 
         });
         binding.btnUnlock.setOnClickListener(v -> {
-            if (!connected) {
+            if (!getViewModel().isConnected()) {
                 showMsg("Please connect doorlock first");
                 return;
             }
             writeESP32("5");
         });
         binding.btnEnroll.setOnClickListener(v -> {
-            if (!connected) {
+            if (!getViewModel().isConnected()) {
                 showMsg("Please connect doorlock first");
                 return;
             }
             writeESP32("1");
         });
         binding.btnEmpty.setOnClickListener(v -> {
-            if (!connected) {
+            if (!getViewModel().isConnected()) {
                 showMsg("Please connect doorlock first");
                 return;
             }
             writeESP32("2");
         });
         binding.btnGetnum.setOnClickListener(v -> {
-            if (!connected) {
+            if (!getViewModel().isConnected()) {
                 showMsg("Please connect doorlock first");
                 return;
             }
@@ -248,29 +214,29 @@ public class DashboardFragment extends Fragment {
 
         });
         binding.btnReadvalue.setOnClickListener((v -> {
-            if (!connected) {
+            if (!getViewModel().isConnected()) {
                 showMsg("Please connect doorlock first");
                 return;
             }
 
-            BluetoothGattService esp32Service = bluetoothService.getGattServiceByUUID(ESP32_USER_SERVICE_UUID);
+            BluetoothGattService esp32Service = getViewModel().getBluetoothService().getGattServiceByUUID(ESP32_USER_SERVICE_UUID);
             if (esp32Service == null)
                 return;
             BluetoothGattCharacteristic rxCharacteristic = esp32Service.getCharacteristic(ESP32_RX_CHARACTERISTIC_UUID);
             BluetoothGattCharacteristic txCharacteristic = esp32Service.getCharacteristic(ESP32_TX_CHARACTERISTIC_UUID);
-            bluetoothService.readCharacteristic(txCharacteristic);
+            getViewModel().getBluetoothService().readCharacteristic(txCharacteristic);
         }));
     }
 
 
     public void writeESP32(String msg) {
-        BluetoothGattService esp32Service = bluetoothService.getGattServiceByUUID(ESP32_USER_SERVICE_UUID);
+        BluetoothGattService esp32Service = getViewModel().getBluetoothService().getGattServiceByUUID(ESP32_USER_SERVICE_UUID);
         if (esp32Service == null)
             return;
         BluetoothGattCharacteristic rxCharacteristic = esp32Service.getCharacteristic(ESP32_RX_CHARACTERISTIC_UUID);
         BluetoothGattCharacteristic txCharacteristic = esp32Service.getCharacteristic(ESP32_TX_CHARACTERISTIC_UUID);
         rxCharacteristic.setValue(msg);
-        bluetoothService.writeCharacteristic(rxCharacteristic);
+        getViewModel().getBluetoothService().writeCharacteristic(rxCharacteristic);
         Log.i(TAG, "rx value: " + new String(rxCharacteristic.getValue()));
     }
 
@@ -294,47 +260,7 @@ public class DashboardFragment extends Fragment {
 
 
 
-    private void displayGattServices(List<BluetoothGattService> gattServices) {
-        if(gattServices == null) return;
-//        String msg = "Service number: " + gattServices.size();
-//        binding.testMsg.setText(msg);
-        String service_info_msg = "";
 
-        BluetoothGattCharacteristic txCharacteristic = null;
-        for (BluetoothGattService gattService : gattServices) {
-            service_info_msg += "Service UUID: " + gattService.getUuid() + "\n";
-            if (!gattService.getUuid().equals(ESP32_USER_SERVICE_UUID))
-                continue;
-
-            List<BluetoothGattCharacteristic> gattCharacteristics = gattService.getCharacteristics();
-
-            int i = 0;
-            for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
-                service_info_msg += "\n" + "Character #" + i + "\n";
-                service_info_msg += "Characteristic UUID: " + gattCharacteristic.getUuid() + "\n";
-                if (gattCharacteristic.getUuid().equals(ESP32_TX_CHARACTERISTIC_UUID)) {
-                    txCharacteristic = gattCharacteristic;
-                }
-                service_info_msg += "Char value: " + gattCharacteristic.getValue() + "\n";
-                i++;
-
-                List<BluetoothGattDescriptor> gattDescriptors = gattCharacteristic.getDescriptors();
-                int j = 0;
-                for (BluetoothGattDescriptor gattDescriptor : gattDescriptors) {
-                    service_info_msg += "Descriptor #" + j + "\n";
-                    service_info_msg += "Descriptor UUID: " + gattDescriptor.getUuid() + "\n";
-                    service_info_msg += "Descriptor value: " + gattDescriptor.getValue() + "\n";
-                    j++;
-                }
-            }
-        }
-        // enable read characteristic
-        if (txCharacteristic != null) {
-            bluetoothService.setCharacteristicNotification(txCharacteristic, true);
-        }
-//        binding.serviceTextInfo.setText(service_info_msg);
-
-    }
 
     public void showMsg(CharSequence msg) {
         Toast toast = Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT);
--- app/src/main/java/com/hthuz/lockcompanion/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/hthuz/lockcompanion/ui/dashboard/DashboardViewModel.java
@@ -1,8 +1,25 @@
 package com.hthuz.lockcompanion.ui.dashboard;
 
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.RequiresPermission;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.hthuz.lockcompanion.BluetoothLeService;
+import com.hthuz.lockcompanion.Values;
+
+import java.util.List;
 
 public class DashboardViewModel extends ViewModel {
 
@@ -10,6 +27,100 @@ public class DashboardViewModel extends ViewModel {
     private final MutableLiveData<String> mState;
     private final MutableLiveData<Boolean> mConnected;
 
+    private static final String TAG = "MY_DEBUG";
+
+    private BluetoothLeService bluetoothService;
+
+    public class ReadRssiThread extends Thread {
+        private volatile boolean running = true;
+        @Override
+        public void run() {
+            while (true) {
+                if (running && isConnected()) {
+                    try {
+                        sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        // Handle interruption
+                    }
+                    Log.i(TAG, "LOOP");
+                    bluetoothService.readRemoteRssi();
+                }
+            }
+        }
+        // Method to stop the thread
+        public void stopThread() {
+            running = false;
+        }
+        public void startThread() {
+            running = true;
+        }
+    }
+    ReadRssiThread readRssiThread;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.i(TAG, "onServiceConnected");
+            bluetoothService = ((BluetoothLeService.LocalBinder) iBinder).getService();
+            if ( bluetoothService != null) {
+                if (!bluetoothService.initialize()) {
+                    Log.e(TAG, "Unable to initialize Bluetooth");
+                }
+                // perform connection
+                final boolean result = bluetoothService.connect(Values.macAddress);
+                Log.d(TAG, "Connect request result=" + result);
+            }
+
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bluetoothService = null;
+        }
+    };
+
+
+    public void displayGattServices(List<BluetoothGattService> gattServices) {
+        if(gattServices == null) return;
+//        String msg = "Service number: " + gattServices.size();
+//        binding.testMsg.setText(msg);
+        String service_info_msg = "";
+
+        BluetoothGattCharacteristic txCharacteristic = null;
+        for (BluetoothGattService gattService : gattServices) {
+            service_info_msg += "Service UUID: " + gattService.getUuid() + "\n";
+            if (!gattService.getUuid().equals(DashboardFragment.ESP32_USER_SERVICE_UUID))
+                continue;
+
+            List<BluetoothGattCharacteristic> gattCharacteristics = gattService.getCharacteristics();
+
+            int i = 0;
+            for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
+                service_info_msg += "\n" + "Character #" + i + "\n";
+                service_info_msg += "Characteristic UUID: " + gattCharacteristic.getUuid() + "\n";
+                if (gattCharacteristic.getUuid().equals(DashboardFragment.ESP32_TX_CHARACTERISTIC_UUID)) {
+                    txCharacteristic = gattCharacteristic;
+                }
+                service_info_msg += "Char value: " + gattCharacteristic.getValue() + "\n";
+                i++;
+
+                List<BluetoothGattDescriptor> gattDescriptors = gattCharacteristic.getDescriptors();
+                int j = 0;
+                for (BluetoothGattDescriptor gattDescriptor : gattDescriptors) {
+                    service_info_msg += "Descriptor #" + j + "\n";
+                    service_info_msg += "Descriptor UUID: " + gattDescriptor.getUuid() + "\n";
+                    service_info_msg += "Descriptor value: " + gattDescriptor.getValue() + "\n";
+                    j++;
+                }
+            }
+        }
+        // enable read characteristic
+        if (txCharacteristic != null) {
+            bluetoothService.setCharacteristicNotification(txCharacteristic, true);
+        }
+//        binding.serviceTextInfo.setText(service_info_msg);
+
+    }
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
         mState = new MutableLiveData<>();
@@ -17,8 +128,17 @@ public class DashboardViewModel extends ViewModel {
         mText.setValue("Lock Companion Unlock");
         mState.setValue("state");
         mConnected.setValue(false);
+        readRssiThread = new ReadRssiThread();
     }
 
+    public BluetoothLeService getBluetoothService() {
+        return bluetoothService;
+    }
+    public ServiceConnection getServiceConnection() { return serviceConnection; }
+
+    public ReadRssiThread getReadRssiThread() {
+        return readRssiThread;
+    }
     public void setState(String state) {
         mState.setValue(state);
     }
